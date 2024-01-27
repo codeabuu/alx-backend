00df22799c399f3dd54fcdbe01298e6174706847
codeabuu <abdulngui718@gmail.com>
2024-01-27 22:50:34 +0000

    function named index_range that takes two integer arguments page and page_size

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,7 +5,8 @@ return in a list for those particular pagination parameters'''
 
 
 def index_range(page, page_size):
-    '''function named index_range that takes two integer arguments page and page_size'''
+    '''function named index_range that takes
+    two integer arguments page and page_size'''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
