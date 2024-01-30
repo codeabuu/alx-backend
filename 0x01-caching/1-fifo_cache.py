ab58d0fc29552e72fb0d47b4489e917a2ecb6ff4
codeabuu <abdulngui718@gmail.com>
2024-01-30 20:13:47 +0000

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,6 +3,7 @@
 
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class FIFOCache(BaseCaching):
     '''FIFO class'''
 
@@ -16,7 +17,7 @@ class FIFOCache(BaseCaching):
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            discarded_key  = next(iter(self.cache_data))
+            discarded_key = next(iter(self.cache_data))
             del self.cache_data[discarded_key]
             print("DISCARD: {}".format(discarded_key))
 
@@ -27,4 +28,3 @@ class FIFOCache(BaseCaching):
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
-
