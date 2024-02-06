afeb5b736859c5d91472bf3c2553b9119bc5d781
codeabuu <abdulngui718@gmail.com>
2024-02-06 12:47:46 +0000

    babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''instantiate the Babel object in your app'''
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask("__name__")
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    '''function to call our index.html'''
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
