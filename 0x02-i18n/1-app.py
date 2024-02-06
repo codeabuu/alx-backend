de75f2c943a345f9566590f8d6820cc249b57f1d
codeabuu <abdulngui718@gmail.com>
2024-02-06 12:57:55 +0000

    babel set up with lan and time

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
-'''instantiate the Babel object in your app'''
-
+'''Instantiate the Babel object in your app'''
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
-
-app = Flask("__name__")
+app = Flask(__name__)
 babel = Babel(app)
 
-
 class Config:
+    '''Configuration class'''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app.config.from_object(Config)
-
 
 @app.route('/')
 def index():
-    '''function to call our index.html'''
+    '''Function to call our index.html'''
     return render_template('1-index.html')
 
 
