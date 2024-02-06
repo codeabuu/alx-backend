5cd98fd5dc1a2bae5b4430c32de997a63dec981d
codeabuu <abdulngui718@gmail.com>
2024-02-06 13:54:11 +0000

    updates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''Instantiate the Babel object in your app'''
 
-from flask import Flask, render_template
-from flask_babel import Babel
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -18,10 +18,17 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    '''Get locale from request'''
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     '''Function to call our index.html'''
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
