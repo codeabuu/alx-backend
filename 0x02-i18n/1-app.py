3dbf305d7af0904a2ac7372be192c789fb62e6e7
codeabuu <abdulngui718@gmail.com>
2024-02-06 12:59:58 +0000

    babel set up with lan and time

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,13 +7,16 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     '''Configuration class'''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
+
 
 @app.route('/')
 def index():
