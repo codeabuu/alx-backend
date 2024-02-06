1d8a3e27bca17af293809b41d1da377dc6d550d9
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-02-06 08:56:26 -0500

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Instantiate the Babel object in your app'''
+"""Instantiate the Babel object in your app."""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config:
-    '''Configuration class'''
+    """Configuration class."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,14 +20,14 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    '''Get locale from request'''
+    """Get locale from request."""
     return request.accept_languages.best_match(
         app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
-    '''Function to call our index.html'''
+    """Function to call our index.html."""
     return render_template('3-index.html')
 
 
